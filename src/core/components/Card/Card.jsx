c10cd08ea03c67b89df32d5528c474ee84c1bb96
0Leonel <leonel20112010@gmail.com>
2023-10-30 11:17:50 -0300

    Elimine las fotos locales

--- src/core/components/Card/Card.jsx
+++ src/core/components/Card/Card.jsx
@@ -12,8 +12,8 @@ export const Card = () => {
         <div className="flex relative">
           <img  alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src={item.url}/>
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">{item.tittle}</h2>
-            <h1 className="title-font text-lg font-medium text-white mb-3">{item.price}</h1>
+            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">{item.price}</h2>
+            <h1 className="title-font text-lg font-medium text-white mb-3">{item.tittle}</h1>
             <p className=" text-xl sm:leading-relaxed ">{item.description}</p>
           </div>
         </div>
--- src/core/components/ProductList/Product.jsx
+++ src/core/components/ProductList/Product.jsx
@@ -7,8 +7,8 @@ export const Product = () => {
     <section className="text-gray-400 bg-gray-900 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Lamparas de madera</h1>
-      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Hechas de madera con la esencia de sudor, sangre y gloria, estas creaciones son un reflejo de la pasión y el sacrificio que se invirtieron en su creación, convirtiéndolas en auténticas obras maestras.</p>
+      <h1 className="sm:text-4xl text-2xl font-medium title-font mb-4 text-white">Lamparas de madera</h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-xl">Hechas de madera con la esencia de sudor, sangre y gloria, estas creaciones son un reflejo de la pasión y el sacrificio que se invirtieron en su creación, convirtiéndolas en auténticas obras maestras.</p>
     </div>
 
     <div className="flex justify-center flex-wrap -m-4">
