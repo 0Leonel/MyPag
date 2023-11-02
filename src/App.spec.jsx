d92f52acbbac3a5c061c7377275a1088ce521291
0Leonel <leonel20112010@gmail.com>
2023-11-02 20:48:32 -0300

    Problemas con el export

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { App } from "./App.jsx";
+import  App  from "./App.jsx";
 
 describe("Home", () => {
   it("Home", () => {
