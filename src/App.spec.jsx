dc3fa9365a7af3a1543700887d827dcc37f88c0a
0Leonel <leonel20112010@gmail.com>
2023-11-02 21:01:05 -0300

    Dios

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -2,8 +2,8 @@ import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import  App  from "./App.jsx";
 
-describe("Home", () => {
-  it("Home", () => {
+describe("", () => {
+  it("", () => {
     const component = render(<App/>);
     expect(component).toBeDefined();
   });
